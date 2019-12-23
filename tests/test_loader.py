72f75930a214dd8244826be281a1399577627c3c
Bart Thate <bthate@dds.nl>
2019-12-23 23:19:02 +0100

    add tests

---
+++ tests/test_loader.py
@@ -0,0 +1,15 @@
+""" loader tests. """
+
+import bl
+import os
+import unittest
+
+class Test_Loader(unittest.TestCase):
+
+    def test_loadmod(self):
+        l = bl.ldr.Loader()
+        l.load_mod("bl.ldr")
+        p = l.save()
+        ll = Loader()
+        ll.load(p)
+        self.assertTrue("bl.ldr" in ll.table)
