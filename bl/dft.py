3175a5c61c2685bc36f56342a98de26999bba223
Bart Thate <bthate@dds.nl>
2020-01-07 01:38:12 +0100

    add init.d

---
+++ bl/dft.py
@@ -0,0 +1,32 @@
+# BOTLIB - Framework to program bots.
+#
+# default values.
+
+from bl.obj import Object
+
+default_irc = {
+    "channel": "",
+    "nick": "botlib",
+    "ipv6": False,
+    "port": 6667,
+    "server": "",
+    "ssl": False,
+    "realname": "botlib",
+    "username": "botlib"
+}
+
+default_krn = {
+    "workdir": "",
+    "kernel": False,
+    "modules": "",
+    "options": "",
+    "prompting": True,
+    "dosave": False,
+    "level": "",
+    "logdir": "",
+    "shell": False
+}
+
+defaults = Object()
+defaults.irc = Object(default_irc)
+defaults.krn = Object(default_krn)
