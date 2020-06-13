0816c0a5aec9abc2b29adbb5fb98714af9df4bb6
Bart Thate <bthate@dds.nl>
2020-06-13 15:21:08 +0200

    update setup

--- setup.py
+++ setup.py
@@ -9,17 +9,17 @@ def read():
 
 setup(
     name='botlib',
-    version='84',
-    url='https://bitbucket.org/botlib/botlib',
+    version='85',
+    url='https://bitbucket.org/bthate/botlib',
     author='Bart Thate',
     author_email='bthate@dds.nl', 
     description=""" BOTLIB is a library you can use to program bots. """,
     long_description=read(),
     long_description_content_type="text/x-rst",
     license='Public Domain',
-    zip_safe=True,
     install_requires=["oklib"],
     packages=["bot"],
+    namespace_packages=["bot"]
     classifiers=['Development Status :: 3 - Alpha',
                  'License :: Public Domain',
                  'Operating System :: Unix',
