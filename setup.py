072f30f4bcc785c49037f00a7cc41bdc6d3a3c5d
Bart Thate <bthate@dds.nl>
2020-06-16 18:11:04 +0200

    up version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read():
 
 setup(
     name='botlib',
-    version='85',
+    version='86',
     url='https://bitbucket.org/bthate/botlib',
     author='Bart Thate',
     author_email='bthate@dds.nl', 
