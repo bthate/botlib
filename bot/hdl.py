fa9855d6074fbe781092890bd31626235a0a4e65
Bart Thate <bthate@dds.nl>
2020-06-16 18:49:25 +0200

    add ok to the mix

---
+++ bot/hdl.py
@@ -0,0 +1,257 @@
+# OKLIB - the ok library !
+#
+# event handler.
+
+import importlib, inspect, os, pkg_resources, queue
+import sys, threading, time, types, _thread
+
+from .obj import Cfg, Default, DoL, Object, get_type
+from .tms import elapsed
+from .thr import launch
+from .trc import get_exception
+from .tms import fntime
+
+def __dir__():
+    return ("Cfg", "Event", "Handler", "Loader")
+
+class Loader(Object):
+
+    def __init__(self):
+        super().__init__()
+        self.cmds = Object()
+        self.table = Object()
+
+    def direct(self, name):
+        return importlib.import_module(name)
+
+    def find_all(self, pkgs="ok"):
+        mns = Object()
+        for p in pkgs.split(","):
+            for mod in self.find_modules(p):
+                mns.update(self.find_names(mod))
+        return mns
+
+    def find_callbacks(self, mod):
+        cbs = {}
+        for key, o in inspect.getmembers(mod, inspect.isfunction):
+            if o.__code__.co_argcount == 2:
+                cbs[key] = o
+        return cbs
+
+    def find_cmds(self, mod):
+        cmds = {}
+        for key, o in inspect.getmembers(mod, inspect.isfunction):
+            if "event" in o.__code__.co_varnames:
+                if o.__code__.co_argcount == 1:
+                    cmds[key] = o
+        return cmds
+
+    def find_modules(self, pkgs, filter=None):
+        mods = []
+        for pkg in pkgs.split(","):
+            if filter and filter not in mn:
+                continue
+            try:
+                p = self.direct(pkg)
+            except ModuleNotFoundError:
+                continue
+            for key, m in inspect.getmembers(p, inspect.ismodule):
+                if m not in mods:
+                    mods.append(m)
+        return mods
+
+    def find_names(self, mod):
+        names = {}
+        for key, o in inspect.getmembers(mod, inspect.isfunction):
+            if "event" in o.__code__.co_varnames:
+                if o.__code__.co_argcount == 1:
+                    names[key] = o.__module__
+        return names
+
+    def find_shorts(self, mn):
+        shorts = DoL()
+        for mod in self.find_modules(mn):
+            for key, o in inspect.getmembers(mod, inspect.isclass):
+                if issubclass(o, Object) and key == o.__name__.lower():
+                    t = "%s.%s" % (o.__module__, o.__name__)
+                    shorts.append(o.__name__.lower(), str(t))
+        return shorts
+
+    def find_types(self, mn):
+        res = []
+        for mod in self.find_modules(mn):
+            for key, o in inspect.getmembers(mod, inspect.isclass):
+                if issubclass(o, Object):
+                    t = "%s.%s" % (o.__module__, o.__name__)
+                    res.append(t)
+        return res
+
+    def get_cmd(self, cmd):
+        for mod in self.table:
+            res = getattr(mod, cmd, None)
+            if not res:
+               continue
+            return res
+
+    def load_mod(self, mn):
+        if mn in self.table:
+            return self.table[mn]
+        self.table[mn] = self.direct(mn)
+        return self.table[mn]
+
+    def walk(self, mns):
+        mods = []
+        for mn in mns.split(","):
+            if not mn:
+                continue
+            try:
+                for x in pkg_resources.resource_listdir(mn, ""):
+                    if x.startswith("_") or not x.endswith(".py"):
+                        continue
+                    module = self.direct("%s.%s" % (mn, x[:-3]))
+                    mods.append(module)
+            except ModuleNotFoundError:
+                continue
+            except TypeError:
+                mod = self.direct(mn)
+                try:
+                    for pkg in mod.__path__:
+                        for x in os.listdir(pkg):
+                            if x.startswith("_") or not x.endswith(".py"):
+                                continue
+                            mmn = "%s.%s" % (mn, x[:-3])
+                            module = self.direct(mmn)
+                            mods.append(module)
+                except AttributeError:
+                    x = mod.__qualname___
+                    if x.startswith("_") or not x.endswith(".py"):
+                        continue
+                    module = self.direct(x)
+                    mods.append(module)
+        return mods
+        
+    def wait(self, nrsec=0):
+        while not self._stopped:
+            time.sleep(0.1)
+
+    def scan(self, pkgs="ok"):
+        res = []
+        for mod in self.walk(pkgs):
+            cmds = self.find_cmds(mod)
+            if cmds:
+                self.load_mod(mod.__name__)
+                res.append(mod)
+                self.cmds.update(cmds)
+        return res
+
+class Handler(Loader):
+ 
+    def __init__(self):
+        super().__init__()
+        self._queue = queue.Queue()
+        self._ready = threading.Event()
+        self._stopped = False
+        self.cbs = Object()
+
+    def handle_cb(self, event):
+        if event.etype in self.cbs:
+            self.cbs[event.etype](self, event)
+        
+    def handler(self):
+        while not self._stopped:
+            e = self._queue.get()
+            if e == None:
+                break
+            self.handle_cb(e)
+
+    def poll(self):
+        raise ENOTIMPLEMENTED
+
+    def put(self, event):
+        self._queue.put(event)
+
+    def ready(self):
+        self._ready.set()
+
+    def register(self, cbname, handler):
+        self.cbs[cbname] = handler        
+
+    def start(self, handler=True):
+        if handler:
+            launch(self.handler)
+
+    def stop(self):
+        self._stopped = True
+        self._queue.put(None)
+
+class Event(Default):
+
+    def __init__(self, txt="", orig=None, origin="", channel=""):
+        super().__init__()
+        self._ready = threading.Event()
+        self._result = []
+        self._thrs = []
+        self.channel = channel
+        self.etype = "event"
+        self.txt = txt
+        self.orig = orig
+        self.origin = origin
+        self.parse()
+        
+    def display(self, o, txt="", keys=None, options="t", post="", strict=False):
+        if not keys:
+            keys = list(o.keys())
+        txt = txt[:]
+        txt += " %s" % self.format(o, keys, strict=strict) 
+        if "t" in options:
+           txt += " %s" % elapsed(time.time() - fntime(o._path))
+        if post:
+           txt += " " + post
+        txt = txt.strip()
+        self.reply(txt)
+
+    def format(self, o, keys=None, strict=False):
+        if keys is None:
+            keys = list(vars(o).keys())
+        res = []
+        txt = ""
+        for key in keys:
+            val = o.get(key)
+            if not val:
+                continue
+            val = str(val)
+            if key == "text":
+                val = val.replace("\\n", "\n")
+            res.append((key, val))
+        for key, val in res:
+            if strict:
+                txt += "%s%s" % (val.strip(), " ")
+            else:
+                txt += "%s=%s%s" % (key, val.strip(), " ")
+        return txt.strip()
+
+    def parse(self):
+        spl = self.txt.split()
+        if spl:
+            self.cmd = spl[0].lower()
+            self.args = spl[1:]
+            self.rest = " ".join(self.args)
+
+    def ready(self):
+        self._ready.set()
+
+    def reply(self, txt):
+        self._result.append(txt)
+ 
+    def show(self):
+        for txt in self._result:
+            print(txt)
+            
+    def wait(self, nrsec=30.0):
+        self._ready.wait(nrsec)
+
+class Command(Event):
+
+    def __init__(self, txt="", orig="", origin="", channel=""):
+        super().__init__(txt, orig, origin, channel)
+        self.etype = "command"
--- bot/log.py
+++ bot/log.py
@@ -1,28 +1,59 @@
-# OKBOT - the ok bot !
+# OKLIB - the ok library !
 #
 #
 
-from ok.obj import Object
-from ok.dbs import Db
+import logging, ok.obj
 
-def __dir__():
-    return ("Log", 'log')
+from .obj import cdir
+from .shl import  touch
 
-class Log(Object):
+def level(loglevel, logfile="", nostream=False):
+    assert ok.obj.workdir
+    if logfile and not os.path.exists(logfile):
+        cdir(logfile)
+        touch(logfile)
+    datefmt = '%H:%M:%S'
+    format_time = "%(asctime)-8s %(message)-70s"
+    format_plain = "%(message)-0s"
+    loglevel = loglevel.upper()
+    logger = logging.getLogger("")
+    if logger.handlers:
+        for handler in logger.handlers:
+            logger.removeHandler(handler)
+    if logger.handlers:
+        for handler in logger.handlers:
+            logger.removeHandler(handler)
+    try:
+        logger.setLevel(loglevel)
+    except ValueError:
+        pass
+    formatter = logging.Formatter(format_plain, datefmt)
+    if nostream:
+        dhandler = DumpHandler()
+        dhandler.propagate = False
+        dhandler.setLevel(loglevel)
+        logger.addHandler(dhandler)
+    else:
+        handler = logging.StreamHandler()
+        handler.propagate = False
+        handler.setFormatter(formatter)
+        try:
+            handler.setLevel(loglevel)
+            logger.addHandler(handler)
+        except ValueError:
+            logging.warning("wrong level %s" % loglevel)
+            loglevel = "ERROR"
+    if logfile:
+        formatter2 = logging.Formatter(format_time, datefmt)
+        filehandler = logging.handlers.TimedRotatingFileHandler(logfile, 'midnight')
+        filehandler.propagate = False
+        filehandler.setFormatter(formatter2)
+        try:
+            filehandler.setLevel(loglevel)
+        except ValueError:
+            pass
+        logger.addHandler(filehandler)
+    return logger
 
-    def __init__(self):
-        super().__init__()
-        self.txt = ""
-
-def log(event):
-    if not event.rest:
-       db = Db()
-       nr = 0
-       for o in db.find("bot.log.Log", {"txt": ""}):
-            event.display(o, str(nr), strict=True)
-            nr += 1
-       return
-    o = Log()
-    o.txt = event.rest
-    o.save()
-    event.reply("ok")
+def rlog(level, txt, extra):
+    logging.log(level, "%s %s" % (txt, extra))
