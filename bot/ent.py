848a199e78eee091240ac0f2e47f1d9aafb30005
Bart Thate <bthate@dds.nl>
2020-08-11 13:10:14 +0200

    use find

--- bot/ent.py
+++ bot/ent.py
@@ -38,14 +38,6 @@ def dne(event):
 
 def log(event):
     if not event.rest:
-        db = Db()
-        res = db.find("bot.ent.Log", {"txt": ""})
-        nr = 0
-        for o in res:
-            event.reply("%s %s %s" % (str(nr), o.txt, elapsed(time.time() - fntime(o.__stamp__))))
-            nr += 1
-        if not nr:
-            event.reply("log what ?")
         return
     l = Log()
     l.txt = event.rest
@@ -53,17 +45,7 @@ def log(event):
     event.reply("ok")
 
 def tdo(event):
-    db = Db()
     if not event.rest:
-        res = db.find("bot.ent.Todo", {"txt": ""})
-        if not res:
-            return
-        nr = 0
-        for o in res:
-            event.reply("%s %s %s" % (str(nr), o.txt, elapsed(time.time() - fntime(o.__stamp__))))
-            nr += 1
-        if not nr:
-            event.reply("do what ?")
         return
     o = Todo()
     o.txt = event.rest
