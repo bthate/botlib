ecb4fba6574347d591db312daff3b94c45ef2ecf
Bart Thate <bthate@dds.nl>
2020-10-21 08:03:36 +0200

    update ol

--- ol/krn.py
+++ ol/krn.py
@@ -15,12 +15,6 @@ starttime = time.time()
 
 class Kernel(ol.hdl.Handler, ol.ldr.Loader):
 
-    classes = ol.Object()
-    cmds = ol.Object()
-    funcs = ol.Object()
-    mods = ol.Object()
-    names = ol.Object()
-
     def __init__(self):
         super().__init__()
         self.ready = threading.Event()
@@ -34,7 +28,7 @@ class Kernel(ol.hdl.Handler, ol.ldr.Loader):
     def cmd(self, txt):
         if not txt:
             return None
-        e = ol.hdl.Event()
+        e = ol.evt.Event()
         e.txt = txt
         ol.bus.bus.add(self)
         self.dispatch(e)
@@ -43,19 +37,23 @@ class Kernel(ol.hdl.Handler, ol.ldr.Loader):
     def direct(self, txt):
         print(txt.rstrip())
 
-    def init(self, mns, exc=""):
+    def init(self, mns, name=None, exc=""):
         if not mns:
             return
         exclude = exc.split(",")
+        thrs = []
         for mn in ol.utl.spl(mns):
             if mn in exclude:
                 continue
+            if "." not in mn:
+                mn = "mods.%s" % mn
             if mn not in self.table:
                 self.load(mn)
             if mn in self.table:
                 func = getattr(self.table[mn], "init", None)
                 if func:
-                    ol.tsk.launch(func, self, name=ol.get_name(func))
+                    thrs.append(ol.tsk.launch(func, self, name=ol.get_name(func)))
+        return thrs
 
     def put(self, e):
         self.queue.put_nowait(e)
@@ -95,48 +93,35 @@ class Kernel(ol.hdl.Handler, ol.ldr.Loader):
 
 kernels = []
 
-def boot(name, wd, md=""):
+def boot(name, wd="", root=False):
+    if root:
+        ol.wd = wd or "/var/lib/%s" % name
+    else:
+        ol.wd = wd or os.path.expanduser("~/.%s" % name)
     cfg = ol.prs.parse_cli()
     k = get_kernel()
     ol.update(k.cfg, cfg)
-    ol.wd = k.cfg.wd = wd
-    k.cfg.md = md or os.path.join(ol.wd, "bmod", "")
-    if "b" in k.cfg.opts:
-        print("%s started at %s" % (name.upper(), time.ctime(time.time()))) 
-        print(ol.format(k.cfg))
+    k.cfg.wd = ol.wd
+    sys.path.insert(0, k.cfg.wd)
     return k
 
-def cmd(txt, wd=None):
-    if not txt:
-        return 
-    global booted
-    if not booted:
-        k = boot("botlib", wd or os.path.expanduser("~/.bot"))
-        booted = True
-    else:
-        k = get_kernel()
-    ol.bus.bus.add(k)
-    if ol.utl.root():
-        scandir(os.path.join(k.cfg.wd, "bmod"), "bmod")
-    e = ol.evt.Event()
-    e.txt = txt
-    k.dispatch(e)
-    return e
+def cmd(txt):
+    k = get_kernel()
+    return k.cmd(txt)
 
 def get_kernel():
     if kernels:
         return kernels[0]
     return Kernel()
 
-def scandir(path, modname="ol"):
+def scandir(path):
     k = get_kernel()
     mods = []
     ol.utl.cdir(path + os.sep + "")
-    sys.path.insert(0, path)
-    for fn in os.listdir(path):
+    for fn in os.listdir(os.path.join(path, "mods")):
         if fn.startswith("_") or not fn.endswith(".py"):
             continue
-        mn = "%s.%s" % (modname, fn[:-3])
+        mn = "mods.%s" % fn[:-3]
         try:
             module = k.load(mn)
         except Exception as ex:
