73b3177923ed7f5a6b70968a7a178893a913ce3a
Bart Thate <bthate@dds.nl>
2020-11-20 00:20:19 +0100

    update etc.

--- test/test_tinder.py
+++ test/test_tinder.py
@@ -1,55 +1,63 @@
-# BOTLIB - the bot library !
-#
-#
+"run all commands"
 
-import ol
+import os, sys ; sys.path.insert(0, os.getcwd())
+
 import os
 import random
 import sys
 import time
 import unittest
 
-param = ol.Object()
+from bot.hdl import Event, Handler
+from bot.obj import Object, get
+from bot.prs import parse_cli
+from bot.thr import launch
+
+cfg = parse_cli()
+verbose = "v" in cfg.opts
+index = cfg.index
+
+param = Object()
 param.add = ["test@shell", "bart"]
 param.dne = ["test4", ""]
-param.edt = ["mymod.rss.Cfg", "mymod.rss.Cfg server=localhost", "mymod.rss.Cfg channel=#dunkbots"]
+param.edt = ["bot.rss.Cfg", "bot.rss.Cfg server=localhost", "bot.rss.Cfg channel=#dunkbots"]
 param.rm = ["reddit", ]
 param.dpl = ["reddit title,summary,link",]
 param.log = ["test1", ""]
 param.flt = ["0", "1", ""]
-param.fnd = ["cfg server==localhost", "kernel mods==rss", "rss rss==reddit", "email From==pvp"]
+param.fnd = ["cfg server==localhost", "kernel wd", "rss rss==reddit rss", "email From==pvp From Subject -t"]
 param.rss = ["https://www.reddit.com/r/python/.rss"]
 param.tdo = ["test4", ""]
 param.mbx = ["~/Desktop/25-1-2013", ""]
 
 events = []
-ignore = ["mbx",]
+ignore = ["mbx", "rss"]
 nrtimes = 1
 
-k = ol.krn.get_kernel()
+k = Handler()
+k.walk("bot")
+k.start()
 
+class Event(Event):
 
-class Event(ol.evt.Event):
-
-    def reply(self, txt):
-        if "v" in k.cfg.opts:
+    def direct(self, txt):
+        if verbose:
             print(txt)
 
 class Test_Tinder(unittest.TestCase):
 
-
     def test_thrs(self):
         thrs = []
-        for x in range(k.cfg.index or 1):
-            ol.tsk.launch(tests, k)
+        for x in range(index or 1):
+            launch(tests, k)
         consume(events)
 
     def test_neuman(self):
-        for x in range(k.cfg.index or 1):
+        for x in range(index or 1):
             tests(k)
 
     def test_sorted(self):
-        for x in range(k.cfg.index or 1):
+        for x in range(index or 1):
             sortedtests(k)
         
 def consume(elems):
@@ -68,14 +76,14 @@ def consume(elems):
     return res
     
 def sortedtests(b):
-    keys = sorted(k.cmds)
+    keys = sorted(k.cbs)
     for cmd in keys:
         if cmd in ignore:
             continue
         events.extend(do_cmd(cmd))
 
 def tests(b):
-    keys = list(k.cmds)
+    keys = list(k.cbs)
     random.shuffle(keys)
     for cmd in keys:
         if cmd in ignore:
@@ -83,7 +91,7 @@ def tests(b):
         events.extend(do_cmd(cmd))
 
 def do_cmd(cmd):
-    exs = ol.get(param, cmd, [""])
+    exs = get(param, cmd, [""])
     e = list(exs)
     random.shuffle(e)
     events = []
@@ -98,4 +106,7 @@ def do_cmd(cmd):
     return events
 
 for e in do_cmd("mbx"):
+    e.wait()
+
+for e in do_cmd("rss https://www.reddit.com/r/python/.rss"):
     e.wait()
