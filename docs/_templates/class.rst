70260239e7d8340a93902dc43b320b5a52b2d3f4
Bart Thate <bart@dds.nl>
2020-12-16 16:24:30 +0100

    update docs

--- docs/_templates/class.rst
+++ docs/_templates/class.rst
@@ -1,5 +1,3 @@
-# GENOCIDE 16 | OTP-CR-117/19/001 | otp.informationdesk@icc-cpi.int | https://genocide.rtfd.io
-
 {{ fullname }}
 {{ underline }}
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read():
 
 setup(
     name='botlib',
-    version='113',
+    version='114',
     url='https://bitbucket.org/bthate/botlib',
     author='Bart Thate',
     author_email='bthate@dds.nl', 
