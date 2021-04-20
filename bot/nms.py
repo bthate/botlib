d029d9573ff7a7a2bdd0e6596fe3e707635ae310
Bart Thate <bthate@dds.nl>
2021-04-20 21:56:49 +0200

    update tbl

--- bot/nms.py
+++ bot/nms.py
@@ -3,6 +3,7 @@
 from .obj import Default, Object
 from .itr import findmods, findnames
 from .utl import direct
+from .zzz import js
 
 class Names(Object):
 
@@ -206,7 +207,6 @@ class Names(Object):
     def getnames(nm, dft=None):
         return Names.names.get(nm, dft)
 
-
     @staticmethod
     def getmodule(mn):
         return Names.modules.get(mn, None)
@@ -220,3 +220,8 @@ class Names(Object):
         Names.names.update(tbl["names"])
         Names.modules.update(tbl["modules"])
         Names.inits.update(tbl["inits"])
+
+    @staticmethod
+    def overload(fn):
+        t = js.loads(s)
+        self.tbl(t)
