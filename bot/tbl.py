ef5628c0cb823412ef13ccacf400a828aa0e5f86
botfather3 <bthate67@gmail.com>
2021-04-30 17:51:03 +0200

    add bot.service

---
+++ bot/tbl.py
@@ -0,0 +1,195 @@
+# This file is placed in the Public Domain.
+
+table = {
+    "inits": {
+        "adm": "bot.adm",
+        "bus": "bot.bus",
+        "clk": "bot.clk",
+        "cmn": "bot.cmn",
+        "dbs": "bot.dbs",
+        "edt": "bot.edt",
+        "evt": "bot.evt",
+        "fnd": "bot.fnd",
+        "hdl": "bot.hdl",
+        "irc": "bot.irc",
+        "log": "bot.log",
+        "nms": "bot.nms",
+        "obj": "bot.obj",
+        "opt": "bot.opt",
+        "prs": "bot.prs",
+        "rss": "bot.rss",
+        "slg": "bot.slg",
+        "tbl": "bot.tbl",
+        "tdo": "bot.tdo",
+        "thr": "bot.thr",
+        "tms": "bot.tms",
+        "trc": "bot.trc",
+        "trm": "bot.trm",
+        "udp": "bot.udp",
+        "url": "bot.url"
+    },
+    "modules": {
+        "cfg": "bot.irc",
+        "cmd": "bot.adm",
+        "dlt": "bot.irc",
+        "dne": "bot.tdo",
+        "dpl": "bot.rss",
+        "flt": "bot.adm",
+        "fnd": "bot.fnd",
+        "ftc": "bot.rss",
+        "krn": "bot.adm",
+        "log": "bot.log",
+        "met": "bot.irc",
+        "mre": "bot.irc",
+        "rem": "bot.rss",
+        "rss": "bot.rss",
+        "slg": "bot.slg",
+        "tdo": "bot.tdo",
+        "thr": "bot.adm",
+        "upt": "bot.adm",
+        "ver": "bot.adm"
+    },
+    "names": {
+        "bus": [
+            "bus.Bus",
+            "bot.bus.Bus"
+        ],
+        "cfg": [
+            "bot.irc.Cfg",
+            "bot.obj.Cfg",
+            "bot.rss.Cfg",
+            "bot.udp.Cfg"
+        ],
+        "client": [
+            "bot.hdl.Client",
+            "hdl.Client"
+        ],
+        "command": [
+            "bot.evt.Command",
+            "evt.Command"
+        ],
+        "dcc": [
+            "bot.irc.DCC"
+        ],
+        "default": [
+            "obj.Default",
+            "bot.obj.Default"
+        ],
+        "enoclass": [
+            "bot.obj.ENOCLASS"
+        ],
+        "enofilename": [
+            "bot.obj.ENOFILENAME"
+        ],
+        "enomore": [
+            "bot.hdl.ENOMORE"
+        ],
+        "enotxt": [
+            "bot.prs.ENOTXT"
+        ],
+        "enouser": [
+            "bot.irc.ENOUSER"
+        ],
+        "event": [
+            "bot.evt.Event",
+            "evt.Event",
+            "bot.irc.Event"
+        ],
+        "feed": [
+            "bot.rss.Feed"
+        ],
+        "fetcher": [
+            "bot.rss.Fetcher"
+        ],
+        "getter": [
+            "bot.prs.Getter"
+        ],
+        "handler": [
+            "bot.hdl.Handler",
+            "hdl.Handler"
+        ],
+        "httperror": [
+            "urllib.error.HTTPError"
+        ],
+        "irc": [
+            "bot.irc.IRC"
+        ],
+        "log": [
+            "bot.log.Log"
+        ],
+        "names": [
+            "nms.Names",
+            "bot.nms.Names"
+        ],
+        "o": [
+            "bot.obj.O"
+        ],
+        "obj": [
+            "bot.obj.Obj"
+        ],
+        "object": [
+            "obj.Object",
+            "bot.obj.Object"
+        ],
+        "objectlist": [
+            "bot.obj.ObjectList",
+            "obj.ObjectList"
+        ],
+        "option": [
+            "bot.prs.Option"
+        ],
+        "output": [
+            "opt.Output",
+            "bot.opt.Output"
+        ],
+        "repeater": [
+            "bot.clk.Repeater",
+            "clk.Repeater"
+        ],
+        "request": [
+            "urllib.request.Request"
+        ],
+        "rss": [
+            "bot.rss.Rss"
+        ],
+        "seen": [
+            "bot.rss.Seen"
+        ],
+        "setter": [
+            "bot.prs.Setter"
+        ],
+        "skip": [
+            "bot.prs.Skip"
+        ],
+        "textwrap": [
+            "bot.irc.TextWrap"
+        ],
+        "thr": [
+            "bot.thr.Thr"
+        ],
+        "timed": [
+            "bot.prs.Timed"
+        ],
+        "timer": [
+            "bot.clk.Timer"
+        ],
+        "todo": [
+            "bot.tdo.Todo"
+        ],
+        "token": [
+            "bot.prs.Token"
+        ],
+        "udp": [
+            "bot.udp.UDP"
+        ],
+        "urlerror": [
+            "urllib.error.URLError"
+        ],
+        "user": [
+            "bot.irc.User"
+        ],
+        "users": [
+            "bot.irc.Users"
+        ]
+    }
+}
