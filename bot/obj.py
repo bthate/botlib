dc405b1c09dadd6734f58314631276ae69e2444e
Bart Thate <bthate67@gmail.com>
2021-05-04 20:34:25 +0200

    add missing

---
+++ bot/obj.py
@@ -0,0 +1,427 @@
+# This file is placed in the Public Domain.
+
+__version__ = 120
+
+## imports ##
+
+import datetime
+import json as js
+import os
+import sys
+import threading
+import time
+import _thread
+import uuid
+
+## defines ##
+
+savelock = _thread.allocate_lock()
+wd = ""
+
+def gettype(o):
+    return str(type(o)).split()[-1][1:-2]
+
+def locked(l):
+    def lockeddec(func, *args, **kwargs):
+        def lockedfunc(*args, **kwargs):
+            l.acquire()
+            res = None
+            try:
+                res = func(*args, **kwargs)
+            finally:
+                l.release()
+            return res
+        lockedfunc.__wrapped__ = func
+        return lockedfunc
+    return lockeddec
+
+class ENOCLASS(Exception):
+
+    pass
+
+class ENOFILENAME(Exception):
+
+    pass
+
+## classes ##
+
+class O:
+
+    __slots__ = ("__dict__", "__stp__")
+
+    def __init__(self):
+        self.__stp__ = os.path.join(gettype(self), str(uuid.uuid4()), os.sep.join(str(datetime.datetime.now()).split()))
+
+    def __delitem__(self, k):
+        try:
+            del self.__dict__[k]
+        except KeyError:
+            pass
+
+    def __getitem__(self, k):
+        return self.__dict__[k]
+
+    def __iter__(self):
+        return iter(self.__dict__)
+
+    def __len__(self):
+        return len(self.__dict__)
+
+    def __lt__(self, o):
+        return len(self) < len(o)
+
+    def __setitem__(self, k, v):
+        self.__dict__[k] = v
+
+    def __repr__(self):
+        return js.dumps(self.__dict__, default=default, sort_keys=True)
+
+    def __str__(self):
+        return str(self.__dict__)
+
+class Obj(O):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        if args:
+            self.update(args[0])
+
+    def get(self, key, default=None):
+        return self.__dict__.get(key, default)
+
+    def keys(self):
+        return self.__dict__.keys()
+
+    def items(self):
+        return self.__dict__.items()
+
+    def merge(self, o):
+        for k, v in self.items():
+            if not o.get(k, None):
+                o[k] = v            
+
+    def register(self, key, value):
+        self[str(key)] = value
+
+    def set(self, key, value):
+        self.__dict__[key] = value
+
+    def update(self, data):
+        return self.__dict__.update(data)
+
+    def values(self):
+        return self.__dict__.values()
+
+class Object(Obj):
+
+    def json(self):
+        return repr(self)
+
+    def load(self, opath):
+        assert wd
+        if opath.count(os.sep) != 3:
+            raise ENOFILENAME(opath)
+        spl = opath.split(os.sep)
+        stp = os.sep.join(spl[-4:])
+        lpath = os.path.join(wd, "store", stp)
+        try:
+            with open(lpath, "r") as ofile:
+                d = js.load(ofile, object_hook=Obj)
+                self.update(d)
+        except FileNotFoundError:
+            pass
+        self.__stp__ = stp
+
+    def save(self, tab=False):
+        assert wd
+        prv = os.sep.join(self.__stp__.split(os.sep)[:2])
+        self.__stp__ = os.path.join(prv, os.sep.join(str(datetime.datetime.now()).split()))
+        opath = os.path.join(wd, "store", self.__stp__)
+        cdir(opath)
+        with open(opath, "w") as ofile:
+            js.dump(self, ofile, default=default, indent=4, sort_keys=True)
+        os.chmod(opath, 0o444)
+        return self.__stp__
+
+class ObjectList(Object):
+
+    def append(self, key, value):
+        if key not in self:
+            self[key] = []
+        if value in self[key]:
+            return
+        if isinstance(value, list):
+            self[key].extend(value)
+        else:
+            self[key].append(value)
+
+    def update(self, d):
+        for k, v in d.items():
+            self.append(k, v)
+
+class Default(Object):
+
+    default = ""
+
+    def __getattr__(self, k):
+        try:
+            return super().__getattribute__(k)
+        except AttributeError:
+            try:
+                return super().__getitem__(k)
+            except KeyError:
+                return self.default
+
+class Cfg(Default):
+
+    pass
+
+## functions ##
+
+def cdir(path):
+    if os.path.exists(path):
+        return
+    res = ""
+    path2, _fn = os.path.split(path)
+    for p in path2.split(os.sep):
+        res += "%s%s" % (p, os.sep)
+        padje = os.path.abspath(os.path.normpath(res))
+        try:
+            os.mkdir(padje)
+            os.chmod(padje, 0o700)
+        except (IsADirectoryError, NotADirectoryError, FileExistsError):
+            pass
+
+def dorepr(o):
+    return '<%s.%s object at %s>' % (
+        o.__class__.__module__,
+        o.__class__.__name__,
+        hex(id(o))
+    )
+
+def getcls(fullname):
+    try:
+        modname, clsname = fullname.rsplit(".", 1)
+    except Exception as ex:
+        raise ENOCLASS(fullname) from ex
+    mod = sys.modules.get(modname, None)
+    return getattr(mod, clsname, None)
+
+def hook(hfn):
+    if hfn.count(os.sep) > 3:
+        oname = hfn.split(os.sep)[-4:]
+    else:
+        oname = hfn.split(os.sep)
+    cname = oname[0]
+    fn = os.sep.join(oname)
+    o = getcls(cname)()
+    o.load(fn)
+    return o
+
+def default(o):
+    if isinstance(o, O):
+        return vars(o)
+    if isinstance(o, dict):
+        return o.items()
+    if isinstance(o, list):
+        return iter(o)
+    if isinstance(o, (type(str), type(True), type(False), type(int), type(float))):
+        return o
+    return dorepr(o)
+
+def fmt(o, keys=None, empty=True, skip=None):
+    if keys is None:
+        keys = o.keys()
+    if not keys:
+        keys = ["txt"]
+    if skip is None:
+        skip = []
+    res = []
+    txt = ""
+    for key in sorted(keys):
+        if key in skip:
+            continue
+        try:
+            val = o[key]
+        except KeyError:
+            continue
+        if empty and not val:
+            continue
+        val = str(val).strip()
+        res.append((key, val))
+    result = []
+    for k, v in res:
+        result.append("%s=%s%s" % (k, v, " "))
+    txt += " ".join([x.strip() for x in result])
+    return txt.strip()
+
+def getname(o):
+    t = type(o)
+    if t == types.ModuleType:
+        return o.__name__
+    try:
+        n = "%s.%s" % (o.__self__.__class__.__name__, o.__name__)
+    except AttributeError:
+        try:
+            n = "%s.%s" % (o.__class__.__name__, o.__name__)
+        except AttributeError:
+            try:
+                n = o.__class__.__name__
+            except AttributeError:
+                n = o.__name__
+    return n
+
+def fntime(daystr):
+    daystr = daystr.replace("_", ":")
+    datestr = " ".join(daystr.split(os.sep)[-2:])
+    try:
+        datestr, rest = datestr.rsplit(".", 1)
+    except ValueError:
+        rest = ""
+    try:
+        t = time.mktime(time.strptime(datestr, "%Y-%m-%d %H:%M:%S"))
+        if rest:
+            t += float("." + rest)
+    except ValueError:
+        t = 0
+    return t
+
+def spl(txt):
+    return [x for x in txt.split(",") if x]
+
+## database ##
+
+def all(otype, selector=None, index=None, timed=None):
+    nr = -1
+    if selector is None:
+        selector = {}
+    for fn in fns(otype, timed):
+        o = hook(fn)
+        if selector and not search(o, selector):
+            continue
+        if "_deleted" in o and o._deleted:
+            continue
+        nr += 1
+        if index is not None and nr != index:
+            continue
+        yield fn, o
+
+def deleted(otype):
+    for fn in fns(otype):
+        o = hook(fn)
+        if "_deleted" not in o or not o._deleted:
+            continue
+        yield fn, o
+
+def every(selector=None, index=None, timed=None):
+    nr = -1
+    if selector is None:
+        selector = {}
+    for otype in os.listdir(os.path.join(wd, "store")):
+        for fn in fns(otype, timed):
+            o = hook(fn)
+            if selector and not search(o, selector):
+                continue
+            if "_deleted" in o and o._deleted:
+                continue
+            nr += 1
+            if index is not None and nr != index:
+                continue
+            yield fn, o
+
+def find(otype, selector=None, index=None, timed=None):
+    if selector is None:
+        selector = {}
+    got = False
+    nr = -1
+    for fn in fns(otype, timed):
+        o = hook(fn)
+        if selector and not search(o, selector):
+            continue
+        if "_deleted" in o and o._deleted:
+            continue
+        nr += 1
+        if index is not None and nr != index:
+            continue
+        got = True
+        yield (fn, o)
+    if not got:
+        return (None, None)
+
+def last(o):
+    path, l = lastfn(str(gettype(o)))
+    if  l:
+        o.update(l)
+    if path:
+        spl = path.split(os.sep)
+        stp = os.sep.join(spl[-4:])
+        return stp
+
+def merge(o):
+    path, l = lastfn(str(gettype(o)))
+    if  l:
+        o.merge(l)
+        o.save()
+    if path:
+        spl = path.split(os.sep)
+        stp = os.sep.join(spl[-4:])
+        return stp
+
+def lastmatch(otype, selector=None, index=None, timed=None):
+    res = sorted(find(otype, selector, index, timed), key=lambda x: fntime(x[0]))
+    if res:
+        return res[-1]
+    return (None, None)
+
+def lasttype(otype):
+    fnn = fns(otype)
+    if fnn:
+        return hook(fnn[-1])
+
+def lastfn(otype):
+    fn = fns(otype)
+    if fn:
+        fnn = fn[-1]
+        return (fnn, hook(fnn))
+    return (None, None)
+
+def fns(name, timed=None):
+    if not name:
+        return []
+    p = os.path.join(wd, "store", name) + os.sep
+    res = []
+    d = ""
+    for rootdir, dirs, _files in os.walk(p, topdown=False):
+        if dirs:
+            d = sorted(dirs)[-1]
+            if d.count("-") == 2:
+                dd = os.path.join(rootdir, d)
+                fls = sorted(os.listdir(dd))
+                if fls:
+                    p = os.path.join(dd, fls[-1])
+                    if timed and "from" in timed and timed["from"] and fntime(p) < timed["from"]:
+                        continue
+                    if timed and timed.to and fntime(p) > timed.to:
+                        continue
+                    res.append(p)
+    return sorted(res, key=fntime)
+
+def listfiles(wd):
+    path = os.path.join(wd, "store")
+    if not os.path.exists(path):
+        return []
+    return sorted(os.listdir(path))
+
+def search(o, s):
+    ok = False
+    try:
+        ss = vars(s)
+    except TypeError:
+        ss = s
+    for k, v in ss.items():
+        vv = getattr(o, k, None)
+        if v not in str(vv):
+            ok = False
+            break
+        ok = True
+    return ok
